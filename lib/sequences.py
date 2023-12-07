eb849153b42f6a79cc9406cbff2d5c9e035ef517
Sheila Chebii <shillerchier055@gmail.com>
2023-12-07 21:04:32 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    x = [0, 1]
+
+    while len(x) < length:
+        a = x[-1] + x[-2]
+        x.append(a)
+
+    print(x[:length])
+
+    
+    
